6e983f83646792e3962151b533d59a50374fa1e9
Qozeem Odeniran <muhdqozeem@gmail.com>
2022-09-03 10:33:35 -0400

    pushing new updates

---
+++ devdotjava/src/devdotjava/FirstCalss.java
@@ -0,0 +1,10 @@
+package devdotjava;
+
+public class FirstCalss {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first Java class");
+
+	}
+
+}
---
+++ w3schools/src/w3schools/Learning.java
@@ -0,0 +1,9 @@
+package w3schools;
+
+public class Learning {
+
+	public static void main(String[] args) {
+		System.out.println("I'd be learning Java on w3schools.com");
+	}
+
+}
