740c2bb8a05fda572bd3f5fd163916a5bbd8c100
qozeemodeniran <Lastborn231>
2023-01-24 20:38:15 -0500

    studied LinkedList

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+// import LinkedList class
+import java.util.LinkedList;
+
+class Main {
+    public static void main(String[] args) {
+        LinkedList<String> languages = new LinkedList<String>();
+        languages.add("Java");
+        languages.add("Pyhton");
+         languages.add("PHP");
+        languages.add("JavaScript");
+
+        System.out.println(languages);
+    }
+}
